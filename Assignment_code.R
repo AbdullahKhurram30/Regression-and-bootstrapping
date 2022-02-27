5d6b7f9cf8b44864ae9d1e1fcd929657636d0042
Abdullah <93071619+AbdullahKhurram30@users.noreply.github.com>
2022-02-27 19:22:35 +0100

    Update Assignment_code.R

--- Assignment_code.R
+++ Assignment_code.R
@@ -78,3 +78,98 @@ rf_MSE
 (rf_MSE)^0.5
 mean(rf_pred)
 quantile(rf_pred, probs = c(0.025, 0.975))
+
+#Predicting values via simulation for the first linear regression model
+# for people with high school degree, the average educ value is 12
+# for people without high school degree, the average educ value is 10
+
+#finding mean of the age
+age_mean <- mean(lalonde$age)
+
+#generating storage vectors for all the simulations
+predicted_degr_1_treat <- rep(0, 1000) #people with high school degrees given the treatment
+predicted_degr_1_notreat <- rep(0, 1000) #people with high school degrees not given the treatment
+predicted_nodegr_1_treat <- rep(0, 1000) #people without high school degrees given the treatment
+predicted_nodegr_1_notreat <- rep(0, 1000) #people without high school degrees not given the treatment
+
+#generating coefficients with simulations
+sim.coeefficients <- sim(lm1, 1000)
+#now the for loops
+#people with degrees given the treatment
+for (i in 1:1000) {
+    predicted_degr_1_treat[i] <- sim.coeffiecients@coef[i,1] + sim.coeffiecients@coef[i,2]*age_mean + sim.coeffiecients@coef[i,3]*12 
+    + sim.coeffiecients@coef[i,4]*1 + sim.coeffiecients@coef[i,5]*0 + sim.coeffiecients@coef[i,6]*0 + sim.coeffiecients@coef[i,7]*0 + 
+    sim.coeffiecients@coef[i,8]*0 + sim.coeffiecients@coef[i,9]*0 + sim.coeffiecients@coef[i,10]*0 + sim.coeffiecients@coef[i,11]*1 + 
+    rnorm(1, mean = 0, sd = sim.coeffiecients@sigma[i]) #the error term
+}
+mean(predicted_degr_1_treat)
+quantile(predicted_degr_1_treat, probs = c(0.025, 0.975))
+#people with degrees not given the treatment
+for (i in 1:1000) {
+    predicted_degr_1_notreat[i] <- sim.coeffiecients@coef[i,1] + sim.coeffiecients@coef[i,2]*age_mean + sim.coeffiecients@coef[i,3]*12 
+    + sim.coeffiecients@coef[i,4]*1 + sim.coeffiecients@coef[i,5]*0 + sim.coeffiecients@coef[i,6]*0 + sim.coeffiecients@coef[i,7]*0 + 
+    sim.coeffiecients@coef[i,8]*0 + sim.coeffiecients@coef[i,9]*0 + sim.coeffiecients@coef[i,10]*0 + sim.coeffiecients@coef[i,11]*0 + 
+    rnorm(1, mean = 0, sd = sim.coeffiecients@sigma[i]) #the error term
+}
+mean(predicted_degr_1_notreat)
+quantile(predicted_degr_1_notreat, probs = c(0.025, 0.975))
+#people without degrees given the treatment
+for (i in 1:1000) {
+    predicted_nodegr_1_treat[i] <- sim.coeffiecients@coef[i,1] + sim.coeffiecients@coef[i,2]*age_mean + sim.coeffiecients@coef[i,3]*10 
+    + sim.coeffiecients@coef[i,4]*1 + sim.coeffiecients@coef[i,5]*0 + sim.coeffiecients@coef[i,6]*0 + sim.coeffiecients@coef[i,7]*1 + 
+    sim.coeffiecients@coef[i,8]*0 + sim.coeffiecients@coef[i,9]*0 + sim.coeffiecients@coef[i,10]*0 + sim.coeffiecients@coef[i,11]*1 + 
+    rnorm(1, mean = 0, sd = sim.coeffiecients@sigma[i]) #the error term
+}
+mean(predicted_nodegr_1_treat)
+quantile(predicted_nodegr_1_treat, probs = c(0.025, 0.975))
+#people without degrees not given the treatment
+for (i in 1:1000) {
+    predicted_nodegr_1_notreat[i] <- sim.coeffiecients@coef[i,1] + sim.coeffiecients@coef[i,2]*age_mean + sim.coeffiecients@coef[i,3]*10 
+    + sim.coeffiecients@coef[i,4]*1 + sim.coeffiecients@coef[i,5]*0 + sim.coeffiecients@coef[i,6]*0 + sim.coeffiecients@coef[i,7]*1 + 
+    sim.coeffiecients@coef[i,8]*0 + sim.coeffiecients@coef[i,9]*0 + sim.coeffiecients@coef[i,10]*0 + sim.coeffiecients@coef[i,11]*0 + 
+    rnorm(1, mean = 0, sd = sim.coeffiecients@sigma[i]) #the error term
+}
+mean(predicted_nodegr_1_treat)
+quantile(predicted_nodegr_1_notreat, probs = c(0.025, 0.975))
+
+
+#Predicting values via simulation for the second linear regression model
+
+#generating storage vectors for all the simulations
+predicted_degr_2_treat <- rep(0, 1000) #people with high school degrees given the treatment
+predicted_degr_2_notreat <- rep(0, 1000) #people with high school degrees not given the treatment
+predicted_nodegr_2_treat <- rep(0, 1000) #people without high school degrees given the treatment
+predicted_nodegr_2_notreat <- rep(0, 1000) #people without high school degrees not given the treatment
+
+#generating coefficients for the models
+sim2 <- sim(lm2, 1000)
+
+# for loops
+# with degrees given treatment
+for (i in 1:1000) {
+    predicted_degr_2_treat[i] <- sim2@coef[i,1] + sim2@coef[i,2]*age_mean + sim2@coef[i,3]*12 + sim2@coef[i,4]*1 + sim2@coef[i,5]*0 + sim2@coef[i,6]*0 + 
+    sim2@coef[i,7]*0 + sim2@coef[i,8]*0 + sim2@coef[i,9]*0 + sim2@coef[i,10]*0 + sim2@coef[i,11]*1 + rnorm(1, mean = 0, sd = sim2@sigma[i]) 
+}
+mean(predicted_degr_2_treat)
+quantile(predicted_degr_2_treat, probs = c(0.025, 0.975))
+# with degrees without treatment
+for (i in 1:1000) {
+    predicted_degr_2_notreat[i] <- sim2@coef[i,1] + sim2@coef[i,2]*age_mean + sim2@coef[i,3]*12 + sim2@coef[i,4]*1 + sim2@coef[i,5]*0 + sim2@coef[i,6]*0 + 
+    sim2@coef[i,7]*0 + sim2@coef[i,8]*0 + sim2@coef[i,9]*0 + sim2@coef[i,10]*0 + sim2@coef[i,11]*0 + rnorm(1, mean = 0, sd = sim2@sigma[i]) 
+}
+mean(predicted_degr_2_notreat)
+quantile(predicted_degr_2_notreat, probs = c(0.025, 0.975))
+#without degrees with treatment
+for (i in 1:1000) {
+    predicted_nodegr_2_treat[i] <- sim2@coef[i,1] + sim2@coef[i,2]*age_mean + sim2@coef[i,3]*10 + sim2@coef[i,4]*1 + sim2@coef[i,5]*0 + sim2@coef[i,6]*0 + 
+    sim2@coef[i,7]*1 + sim2@coef[i,8]*0 + sim2@coef[i,9]*0 + sim2@coef[i,10]*0 + sim2@coef[i,11]*1 + rnorm(1, mean = 0, sd = sim2@sigma[i]) 
+}
+mean(predicted_nodegr_2_treat)
+quantile(predicted_nodegr_2_treat, probs = c(0.025, 0.975))
+#without degrees without treatment
+for (i in 1:1000) {
+    predicted_nodegr_2_notreat[i] <- sim2@coef[i,1] + sim2@coef[i,2]*age_mean + sim2@coef[i,3]*10 + sim2@coef[i,4]*1 + sim2@coef[i,5]*0 + sim2@coef[i,6]*0 + 
+    sim2@coef[i,7]*1 + sim2@coef[i,8]*0 + sim2@coef[i,9]*0 + sim2@coef[i,10]*0 + sim2@coef[i,11]*0 + rnorm(1, mean = 0, sd = sim2@sigma[i]) 
+}
+mean(predicted_nodegr_1_notreat)
+quantile(predicted_nodegr_1_notreat, probs = c(0.025, 0.975))
